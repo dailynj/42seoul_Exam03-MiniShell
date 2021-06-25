a0263ba0c9a329e1ec7303a30b40f5c5fcb25c76
su <wkdlfflxh@naver.com>
2021-06-26 01:31:53 +0900

    [FIX] makefile spinner

--- builtin/a.sh
+++ builtin/a.sh
@@ -1,12 +1,13 @@
 i=1
 j=0
-max=10000
+max=20000
 sp="/-\|"
-echo -n "SUNASHELL is Ready " 
+printf "\033[0;31m"
+echo -n "Loading... "
 echo -n ' '
 while [ $j -lt $max ]
 do
     printf "\b${sp:i++%${#sp}:1}"
     j=$(($j+1))
 done
-printf "\b✅"
+
